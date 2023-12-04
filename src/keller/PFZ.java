c755d776405890a2cda721e9895e612651c4d8a4
Malte Blanke <blanke@ursulaschule.de>
2023-12-04 11:10:28 +0100

    klausuraufgaben

---
+++ src/keller/PFZ.java
@@ -0,0 +1,43 @@
+package keller;
+
+public class PFZ {
+    public static boolean istPrim(int n) {
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            if( n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static intKeller pfz2(int n) {
+        intKeller s = new intZeigerKeller();
+        for(int i=2; i<n; i++) {
+            if( istPrim(i) ) {
+                int k = n;
+                while ( k % i == 0 ) {
+                    s.push(i);
+                    k = k / i;
+                }
+            }
+        }
+        return s;
+    }
+
+    public static void reduziere() {
+
+
+    }
+    public static intKeller zStapel = pfz2(420);
+    public static intKeller nStapel = pfz2(450);
+
+    public static void main(String[] argv) {
+        System.out.println( zStapel );
+        System.out.println( nStapel );
+
+        reduziere();
+
+        System.out.println( zStapel );
+        System.out.println( nStapel );
+    }
+}
